b913e01753dba0ec4b3d0e011967ef06fd1e7146
Samuel-dot-cloud <successam336@gmail.com>
2020-09-01 17:53:24 +0300

    Beginning challenge

--- 4-DOM-pig-game/starter/challenges.js
+++ 4-DOM-pig-game/starter/challenges.js
@@ -1,15 +1,7 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-*/
 var scores, roundScore, activePlayer, gamePlaying;
 init();
 
+var lastDice;
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     if (gamePlaying) {
@@ -22,7 +14,12 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         diceDOM.src = 'dice-' + dice + '.png';
 
         //3. Update round score IF rolled number was NOT 1
-        if (dice !== 1) {
+        if (dice === 6 && lastDice === 6) {
+            //player loses score
+            scores[activePlayer] = 0;
+            document.querySelector('#current-' + activePlayer).textContent = scores[activePlayer];
+            nextPlayer();
+        } else if (dice !== 1) {
             // add score
             roundScore += dice;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
@@ -30,6 +27,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
             // Next player
             nextPlayer();
         }
+        lastDice = dice;
     }
 });
 
@@ -41,9 +39,19 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         // Update UI
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+        var input = document.querySelector('.final-score').value;
+        var winningScore;
+
+        // Undefined, 0, null, or "" are COERCED to false
+        //Anything else is coerced to true
+        if (input) {
+            winningScore = input;
+        } else {
+            winningScore = 100;
+        }
 
         // Check if player won the game
-        if (scores[activePlayer] >= 100) {
+        if (scores[activePlayer] >= winningScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
@@ -92,10 +100,3 @@ function init() {
     document.querySelector('.player-1-panel').classList.remove('active');
 
 }
-
-
-
-//document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-//var x = document.querySelector('#score-0').textContent;
-//console.log(x);
